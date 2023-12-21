b530ac56d2fc610f167a3530ad10e1cd0469260f
Jimena Taleb <jime-taleb@hotmail.com>
2023-12-21 09:26:42 -0300

    adding render operations function

--- js/main.js
+++ js/main.js
@@ -29,3 +29,23 @@ const getData = (key) => JSON.parse(localStorage.getItem(key))
 
 //Funcion setear la info de los arrays (operaciones, categorias) desde local storage
 const setData = (key, data) => localStorage.setItem(key, JSON.stringify(data))
+
+// Funciones principales
+
+//Renderizar las operaciones en la tabla 
+const renderOperations = (operations) => {
+    for (const operation of operations) {
+      $("#operations--table-body").innerHTML += `
+      <tr class="flex flex-wrap justify-between lg:flex-nowrap lg:items-center">
+         <td class="w-1/2 text-base mt-4">${operation.description}</td>
+         <td class="w-1/2 text-xs mt-4 text-right lg:text-center"><span class="my-1 rounded bg-green-100 mt-4">${operation.category}</span></td>
+         <td class="hidden lg:flex lg:w-1/2 lg:text-center">${operation.date}</td>
+         <td class="w-1/2 text-2xl mt-4 lg:text-right">${operation.amount}</td>
+         <td class="w-1/2 text-right lg:text-right">
+             <button onclick="showFormEdit('${operation.id}')"><i class="fa-regular fa-pen-to-square text-xs mt-4 bg-green-500 text-white py-1 px-2 rounded-md ml-2"></i></button>
+             <button><i class="fa-solid fa-trash text-xs mt-4 bg-red-500 text-white py-1 px-2 rounded-md ml-2"></i></button>
+         </td>
+      </tr>
+      `;
+    }
+  };
