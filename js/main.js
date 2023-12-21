b7c06bc3da19db9d34e0fbaa51b15c8c281074c9
Jimena Taleb <jime-taleb@hotmail.com>
2023-12-21 09:19:11 -0300

    adding show element function

--- js/main.js
+++ js/main.js
@@ -2,3 +2,10 @@
 
 //Funcion selector
 const $ = (selector) => document.querySelector(selector);
+
+//Funcion mostrar elementos
+const showElement = (selectors) => {
+    for (const selector of selectors){
+      $(selector).classList.remove("hidden")
+    }
+  }
