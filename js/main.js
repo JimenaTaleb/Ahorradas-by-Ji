6283e156a681e71b60efc5c2bc02006ef5024512
Jimena Taleb <jime-taleb@hotmail.com>
2023-12-27 12:38:57 -0300

    adding styles to operation amount

--- js/main.js
+++ js/main.js
@@ -72,12 +72,16 @@ const renderOperations = (operations) => {
     hideElement(["#section--operations-no-results"])
     for (const operation of operations) {
       const categorySelected = getData("categories").find(category => category.id === operation.category)
+
+      const amountClass = operation.type === "ganancia" ? 'text-green-500' : 'text-red-500';
+      const amountSign = operation.type === "ganancia" ? '+$' : '-$';
+
       $("#operations--table-body").innerHTML += `
       <tr class="flex flex-wrap justify-between lg:flex-nowrap lg:items-center">
          <td class="w-1/2 text-base mt-4">${operation.description}</td>
          <td class="w-1/2 text-xs mt-4 text-right lg:text-center"><span class="my-1 rounded bg-green-100 mt-4">${categorySelected.categoryName}</span></td>
          <td class="hidden lg:flex lg:w-1/2 lg:text-center justify-center">${new Date(operation.date).toLocaleDateString('es-ES')}</td>
-         <td class="w-1/2 text-2xl mt-4 lg:text-center">${operation.amount}</td>
+         <td class="w-1/2 text-base mt-4 lg:text-center font-bold ${amountClass}">${amountSign}${operation.amount}</td>
          <td class="w-1/2 text-right lg:text-center">
              <button onclick="showFormEdit('${operation.id}')"><i class="fa-regular fa-pen-to-square text-xs mt-4 bg-green-500 text-white py-1 px-2 rounded-md ml-2 hover:bg-green-600"></i></button>
              <button onclick="showModalDeleteOperation('${operation.id}', '${operation.description}')"><i class="fa-solid fa-trash text-xs mt-4 bg-red-500 text-white py-1 px-2 rounded-md ml-2 hover:bg-red-600"></i></button>
